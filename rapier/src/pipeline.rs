8f1b60923586cdea854d472f795f3a262a0ef0e0
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-02-08 19:21:19 +0100

    Use IntersectionEvent instead of ProximityEvent

--- rapier/src/pipeline.rs
+++ rapier/src/pipeline.rs
@@ -6,10 +6,10 @@ use crossbeam::channel::Receiver;
 use heron_core::{CollisionEvent, Gravity};
 
 use crate::convert::IntoRapier;
-use crate::rapier::data::arena::Index;
 use crate::rapier::dynamics::{IntegrationParameters, JointSet, RigidBodySet};
-use crate::rapier::geometry::{BroadPhase, ColliderSet, ContactEvent, NarrowPhase, ProximityEvent};
-use crate::rapier::ncollide::query::Proximity;
+use crate::rapier::geometry::{
+    BroadPhase, ColliderHandle, ColliderSet, ContactEvent, IntersectionEvent, NarrowPhase,
+};
 use crate::rapier::pipeline::{ChannelEventCollector, PhysicsPipeline};
 
 #[allow(clippy::too_many_arguments)]
@@ -44,7 +44,7 @@ pub(crate) fn step(
 
 pub(crate) struct EventManager {
     contacts: Receiver<ContactEvent>,
-    proximities: Receiver<ProximityEvent>,
+    intersections: Receiver<IntersectionEvent>,
     handler: ChannelEventCollector,
 }
 
@@ -56,7 +56,7 @@ impl Default for EventManager {
         Self {
             contacts,
             handler,
-            proximities,
+            intersections: proximities,
         }
     }
 }
@@ -78,29 +78,28 @@ impl EventManager {
             }
         }
 
-        while let Ok(ProximityEvent {
+        while let Ok(IntersectionEvent {
             collider1,
             collider2,
-            prev_status,
-            new_status,
-        }) = self.proximities.try_recv()
+            intersecting,
+        }) = self.intersections.try_recv()
         {
             if let Some((e1, e2)) = Self::entity_pair(colliders, collider1, collider2) {
-                match (prev_status, new_status) {
-                    (_, Proximity::Intersecting) => {
-                        events.send(CollisionEvent::Started(e1, e2));
-                    }
-                    (Proximity::Intersecting, _) => {
-                        events.send(CollisionEvent::Stopped(e1, e2));
-                    }
-                    _ => {}
+                if intersecting {
+                    events.send(CollisionEvent::Started(e1, e2));
+                } else {
+                    events.send(CollisionEvent::Stopped(e1, e2));
                 }
             }
         }
     }
 
     #[allow(clippy::cast_possible_truncation)]
-    fn entity_pair(colliders: &ColliderSet, h1: Index, h2: Index) -> Option<(Entity, Entity)> {
+    fn entity_pair(
+        colliders: &ColliderSet,
+        h1: ColliderHandle,
+        h2: ColliderHandle,
+    ) -> Option<(Entity, Entity)> {
         if let (Some(b1), Some(b2)) = (colliders.get(h1), colliders.get(h2)) {
             let e1 = Entity::from_bits(b1.user_data as u64);
             let e2 = Entity::from_bits(b2.user_data as u64);
@@ -116,7 +115,6 @@ mod tests {
     use crate::pipeline::EventManager;
     use crate::rapier::dynamics::RigidBodyBuilder;
     use crate::rapier::geometry::{ColliderBuilder, ColliderHandle};
-    use crate::rapier::ncollide::pipeline::ProximityEvent;
     use crate::rapier::pipeline::EventHandler;
 
     use super::*;
@@ -202,16 +200,17 @@ mod tests {
     }
 
     #[test]
-    fn proximity_disjoint_to_intersection_fires_collision_started() {
+    fn intersection_true_fires_collision_started() {
         let manager = EventManager::default();
         let context = TestContext::default();
 
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::Disjoint,
-            Proximity::Intersecting,
-        ));
+        manager
+            .handler
+            .handle_intersection_event(IntersectionEvent::new(
+                context.handle1,
+                context.handle2,
+                true,
+            ));
 
         let mut events = Events::<CollisionEvent>::default();
         manager.fire_events(&context.colliders, &mut events);
@@ -224,86 +223,17 @@ mod tests {
     }
 
     #[test]
-    fn proximity_intersection_to_disjoint_fires_collision_stopped() {
+    fn intersection_false_fires_collision_stopped() {
         let manager = EventManager::default();
         let context = TestContext::default();
 
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::Intersecting,
-            Proximity::Disjoint,
-        ));
-
-        let mut events = Events::<CollisionEvent>::default();
-        manager.fire_events(&context.colliders, &mut events);
-        let events: Vec<CollisionEvent> = events.get_reader().iter(&events).cloned().collect();
-
-        assert_eq!(
-            events,
-            vec![CollisionEvent::Stopped(context.entity1, context.entity2)]
-        );
-    }
-
-    #[test]
-    fn proximity_within_margin_to_disjoint_and_vice_versa_does_nothing() {
-        let manager = EventManager::default();
-        let context = TestContext::default();
-
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::WithinMargin,
-            Proximity::Disjoint,
-        ));
-
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::Disjoint,
-            Proximity::WithinMargin,
-        ));
-
-        let mut events = Events::<CollisionEvent>::default();
-        manager.fire_events(&context.colliders, &mut events);
-        let events: Vec<CollisionEvent> = events.get_reader().iter(&events).cloned().collect();
-
-        assert!(events.is_empty());
-    }
-
-    #[test]
-    fn proximity_with_margin_to_intersecting_fires_collision_started() {
-        let manager = EventManager::default();
-        let context = TestContext::default();
-
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::WithinMargin,
-            Proximity::Intersecting,
-        ));
-
-        let mut events = Events::<CollisionEvent>::default();
-        manager.fire_events(&context.colliders, &mut events);
-        let events: Vec<CollisionEvent> = events.get_reader().iter(&events).cloned().collect();
-
-        assert_eq!(
-            events,
-            vec![CollisionEvent::Started(context.entity1, context.entity2)]
-        );
-    }
-
-    #[test]
-    fn proximity_intersecting_to_within_margin_fires_collision_stopped() {
-        let manager = EventManager::default();
-        let context = TestContext::default();
-
-        manager.handler.handle_proximity_event(ProximityEvent::new(
-            context.handle1,
-            context.handle2,
-            Proximity::Intersecting,
-            Proximity::WithinMargin,
-        ));
+        manager
+            .handler
+            .handle_intersection_event(IntersectionEvent::new(
+                context.handle1,
+                context.handle2,
+                false,
+            ));
 
         let mut events = Events::<CollisionEvent>::default();
         manager.fire_events(&context.colliders, &mut events);
