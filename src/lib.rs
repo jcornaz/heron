9489b5e9fd88639e619819aefe65ec23a4b1c556
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-03-02 22:50:57 +0100

    Update docs

--- src/lib.rs
+++ src/lib.rs
@@ -59,7 +59,7 @@
 //!         // Spawn any bundle of your choice. Only make sure there is a `GlobalTransform`
 //!         .spawn(SpriteBundle::default())
 //!
-//!        // Make it a physics body, by attaching a collision shape
+//!         // Make it a physics body, by attaching a collision shape
 //!         .with(Body::Sphere { radius: 10.0 })
 //!
 //!         // Optionally define a type (if absent, the body will be *dynamic*)
@@ -89,7 +89,7 @@
 //!
 //! * The different [`BodyType`] (dynamic, static or sensor)
 //! * How to define the world's [`Gravity`]
-//! * How to define the [`Restitution`] of a rigid body
+//! * How to define the [`PhysicsMaterial`]
 //! * How to listen to [`CollisionEvent`]
 
 use bevy_app::{AppBuilder, Plugin};
