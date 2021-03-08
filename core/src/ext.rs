d8cf8cb0dc1a4755f71f507eb0aa1fa621534df7
MGlolenstine <MGlolenstine@gmail.com>
2021-03-08 10:16:33 +0100

    Changed cargo fmt's mind about not liking me.

--- core/src/ext.rs
+++ core/src/ext.rs
@@ -10,7 +10,7 @@ pub trait AppBuilderExt {
     /// Add a system to the "physics update" stage so that it runs before each physics step.
     ///
     /// This can be used to add systems that modify transform/velocity or other physics components.
-    ///
+    ///    
     /// Typically (and by default) physics steps run at a fixed rate and are out of sync with the bevy update.	 
     fn add_physics_system<S: System<In = (), Out = ()>>(&mut self, system: S) -> &mut Self;
 }
