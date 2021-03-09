8a03dea8d4c927a088a184c4987b14eed4dffe8d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2021-03-09 20:49:21 +0100

    Write case for locking after creation (not implemented)

--- rapier/tests/constraints.rs
+++ rapier/tests/constraints.rs
@@ -1,16 +1,11 @@
-#![cfg(all(
-    any(feature = "2d", feature = "3d"),
-    not(all(feature = "2d", feature = "3d")),
-))]
+#![cfg(feature = "2d")]
 
 use bevy::core::CorePlugin;
 use bevy::prelude::*;
 use bevy::reflect::TypeRegistryArc;
 
-use heron_core::{Body, PhysicMaterial, RotationConstraints};
-use heron_rapier::convert::IntoBevy;
+use heron_core::{Body, RotationConstraints};
 use heron_rapier::rapier::dynamics::{IntegrationParameters, RigidBodySet};
-use heron_rapier::rapier::math::AngVector;
 use heron_rapier::{BodyHandle, RapierPlugin};
 
 fn test_app() -> App {
@@ -26,7 +21,6 @@ fn test_app() -> App {
 }
 
 #[test]
-#[cfg(feature = "2d")]
 fn rotation_is_not_constrained_without_the_component() {
     let mut app = test_app();
 
@@ -48,8 +42,7 @@ fn rotation_is_not_constrained_without_the_component() {
 }
 
 #[test]
-#[cfg(feature = "2d")]
-fn rotation_can_be_locked_on_creation() {
+fn rotation_can_be_locked_at_creation() {
     let mut app = test_app();
 
     let entity = app.world.spawn((
@@ -57,6 +50,34 @@ fn rotation_can_be_locked_on_creation() {
         Body::Sphere { radius: 10.0 },
         RotationConstraints::lock(),
     ));
+
+    app.update();
+
+    let bodies = app.resources.get::<RigidBodySet>().unwrap();
+
+    assert_eq!(
+        bodies
+            .get(app.world.get::<BodyHandle>(entity).unwrap().rigid_body())
+            .unwrap()
+            .effective_world_inv_inertia_sqrt,
+        0.0
+    );
+}
+
+#[test]
+#[ignore]
+fn rotation_can_be_locked_after_creation() {
+    let mut app = test_app();
+
+    let entity = app
+        .world
+        .spawn((GlobalTransform::default(), Body::Sphere { radius: 10.0 }));
+
+    app.update();
+
+    app.world
+        .insert_one(entity, RotationConstraints::lock())
+        .unwrap();
 
     app.update();
 
